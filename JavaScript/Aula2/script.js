3d59fecb7412b809af989ff4925ea35b58748cae
rodrigo-fari <rodrigofal44@gmail.com>
2025-07-30 17:26:15 +0100

    Added exercises from todays lesson ;p

--- JavaScript/Aula2/script.js
+++ JavaScript/Aula2/script.js
@@ -48,49 +48,142 @@ console.log(person.job); OUTP: Undefined!
 */
 
 //ex01
+function ex01() {
 
-let city = [
-	"Porto",
-	"Vila Nova de Gaia",
-	"Maia"
-];
+	let city = [
+		"Porto",
+		"Vila Nova de Gaia",
+		"Maia"
+	];
 
-console.log("Size = " + city.length);
+	console.log("Size = " + city.length);
 
-for(let i = 0; i < city.length; i++) {
-	console.log("Index [" + i + "] = " + city[i]);
+	for (let i = 0; i < city.length; i++) {
+		console.log("Index [" + i + "] = " + city[i]);
+	}
+	console.log("\nFirst city = " + city[0]);
+	console.log("\nMiddle index = " + city[1]);
+
+	console.log("\nBefore change:");
+	for (let i = 0; i < city.length; i++) {
+		console.log("Index [" + i + "] = " + city[i]);
+	}
+
+	city[1] = "Gondomar"
+	console.log("\nAfter change:");
+	for (let i = 0; i < city.length; i++) {
+		console.log("Index [" + i + "] = " + city[i]);
+	}
+
+	city.push("Leiria");
+	console.log("\nAdded new index to array:");
+	for (let i = 0; i < city.length; i++) {
+		console.log("Index [" + i + "] = " + city[i]);
+	}
+
+	console.log("New size = " + city.length);
+
+	city.pop("Leiria");
+	console.log("\nRemoved last index of the array:");
+	for (let i = 0; i < city.length; i++) {
+		console.log("Index [" + i + "] = " + city[i]);
+	}
+
+	console.log("New size = " + city.length);
+
+	console.log("\nFinal array's version:");
+	for (let i = 0; i < city.length; i++) {
+		console.log("Index [" + i + "] = " + city[i]);
+	}
 }
-console.log("\nFirst city = " + city[0]);
-console.log("\nMiddle index = " + city[1]);
 
-console.log("\nBefore change:");
-for(let i = 0; i < city.length; i++) {
-	console.log("Index [" + i + "] = " + city[i]);
+function ex02() {
+	let animals = [
+		"Cão",
+		"Gato",
+		"Coelho",
+		"Pássaro",
+		"Peixe"
+	];
+
+	console.log("--| New array:");
+	for (let i = 0; i < animals.length; i++) {
+		console.log("Index [" + i + "] = " + animals[i]);
+	}
+
+	console.log("\n--| Remove last array item");
+	animals.pop();
+	console.log("New array:");
+	for (let i = 0; i < animals.length; i++) {
+		console.log("Index [" + i + "] = " + animals[i]);
+	}
+
+	console.log("\n--| Add item at the end");
+	animals.push("Hamster");
+	console.log("New array:");
+	for (let i = 0; i < animals.length; i++) {
+		console.log("Index [" + i + "] = " + animals[i]);
+	}
+
+	console.log("\n--| Remove first array item");
+	animals.shift();
+	console.log("New array:");
+	for (let i = 0; i < animals.length; i++) {
+		console.log("Index [" + i + "] = " + animals[i]);
+	}
+
+	console.log("\n--| Add new first array item");
+	animals.unshift("Cavalo");
+	console.log("New array:");
+	for (let i = 0; i < animals.length; i++) {
+		console.log("Index [" + i + "] = " + animals[i]);
+	}
+
+	console.log("\n--| Changind index 2");
+	animals[2] = "Tartaruga";
+	console.log("New array:");
+	for (let i = 0; i < animals.length; i++) {
+		console.log("Index [" + i + "] = " + animals[i]);
+	}
+
+	console.log("\n--| Single lined array:" + animals);
 }
 
-city[1] = "Gondomar"
-console.log("\nAfter change:");
-for(let i = 0; i < city.length; i++) {
-	console.log("Index [" + i + "] = " + city[i]);
+function ex03() {
+	let carro = {
+		marca: "Opel",
+		modelo: "Corsa",
+		ano: "1998",
+		cor: "Azul"
+	};
+
+	console.log("Original OBJ:");
+	console.log(carro);
+	console.log("\n");
+
+	console.log("Chaging color to black...")
+	carro.cor = "preto";
+	console.log("\n");
+
+	console.log("Updated OBJ:");
+	console.log(carro);
+	console.log("\n");
+
+	console.log("Adding new key:value...");
+	carro.quilometragem = "999999km";
+	console.log("\n");
+
+	console.log("Updated OBJ:");
+	console.log(carro);
+	console.log("\n");
+
+	console.log("Deleting new key:value...");
+	delete carro.quilometragem;
+	console.log("\n");
+
+	console.log("Updated OBJ:");
+	console.log(carro);
+	console.log("\n");
 }
 
-city.push("Leiria");
-console.log("\nAdded new index to array:");
-for(let i = 0; i < city.length; i++) {
-	console.log("Index [" + i + "] = " + city[i]);
-}
-
-console.log("New size = " + city.length);
-
-city.pop("Leiria");
-console.log("\nRemoved last index of the array:");
-for(let i = 0; i < city.length; i++) {
-	console.log("Index [" + i + "] = " + city[i]);
-}
-
-console.log("New size = " + city.length);
-
-console.log("\nFinal array's version:");
-for(let i = 0; i < city.length; i++) {
-	console.log("Index [" + i + "] = " + city[i]);
-}
+ex03();
